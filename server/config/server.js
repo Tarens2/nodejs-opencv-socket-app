9fa4b2b611848e6ff0b016531e62a2d779b80370
Alekhin Taras <tarasAKK@mail.ru>
2018-02-12 17:03:38 +0300

    fix

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,7 @@
 var path = require('path')
+const PORT = process.env.PORT || 5000
 
 module.exports = {
-  httpPort: 8080,
+  httpPort: PORT,
   staticFolder: path.join(__dirname + '/../../client')
 };
